1b75d6d668d43730b53032386eb4533e8f8e4d84
Vitaly Malyshev <vitaly.m88@yandex.ru>
2024-03-25 19:32:21 +0400

    Всё кроме отладки. PS не работает страничка с листингом, т.к. в базе дубликаты, ну и пусть

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -11,6 +11,10 @@ from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+# Или вместо 404 можно импортируем ошибку доступа - 
+# для показа юзеру при попытке отредактировать не свой пост:
+# from django.core.exceptions import PermissionDenied
 
 
 class BirthdayListView(ListView):
@@ -33,11 +37,36 @@ class BirthdayCreateView(LoginRequiredMixin, CreateView):
 class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
+    # При работе с CBV для выполнения этой проверки нужно переопределить метод dispatch(),
+    # который работает как диспетчер, перенаправляя запросы согласно их HTTP-методам:
+    # GET-запрос к методу CBV get(), POST-запрос к методу post() и так далее.
+    # Если пользователь, выполняющий запрос, не является автором объекта, то и пускать его дальше не надо:
+    def dispatch(self, request, *args, **kwargs):
+        # Получаем объект по первичному ключу и автору или вызываем 404 ошибку.
+        get_object_or_404(Birthday, pk=kwargs['pk'], author=request.user)
+        # Если объект был найден, то вызываем родительский метод, 
+        # чтобы работа CBV продолжилась.
+        return super().dispatch(request, *args, **kwargs)
+    
+    # либо так - с PermissionDenied(выбор зависит от требований к проекту)
+    # def dispatch(self, request, *args, **kwargs):
+        # При получении объекта не указываем автора.
+        # Результат сохраняем в переменную.
+        instance = get_object_or_404(Birthday, pk=kwargs['pk'])
+        # Сверяем автора объекта и пользователя из запроса.
+        if instance.author != request.user:
+            # Здесь может быть как вызов ошибки, так и редирект на нужную страницу.
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
 
 
 class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
+
+    def dispatch(self, request, *args, **kwargs):
+        get_object_or_404(Birthday, pk=kwargs['pk'], author=request.user)
+        return super().dispatch(request, *args, **kwargs)
 
 
 class BirthdayDetailView(DetailView):
